64157219bf4d98b57cd9b9bc74723474d5cee823
Lol <122758988+Loled69@users.noreply.github.com>
2023-01-19 19:07:11 -0300

    Update lemone.lua

--- lemone.lua
+++ lemone.lua
@@ -10,24 +10,6 @@ nlprice2 = gameplay.get_hash_key("MP1_PROP_NIGHTCLUB_VALUE")
   end
  end
 
-local function ezBanCustom()
-nlprice = gameplay.get_hash_key("MP0_PROP_NIGHTCLUB_VALUE")
-nlprice2 = gameplay.get_hash_key("MP1_PROP_NIGHTCLUB_VALUE")
-        local input_stat, input_val = input.get("Enter value (Max 2147483647)", "", 10, 3)
-    if input_stat == 1 then
-        return HANDLER_CONTINUE
-    end
-    if input_stat == 2 then
-        return HANDLER_POP
-    end
-   local leplayer = script.get_global_i(1574918)
-  if leplayer == 0 then
-    stats.stat_set_int(nlprice, ((input_val*2) + 4500000), true)
-  else
-    stats.stat_set_int(nlprice2, ((input_val*2) + 4500000), true)
-  end
- end
-
 
 moneyManu = menu.add_feature("mone stuffz", "parent", 0)
 menu.add_feature("READ ME", "action", moneyManu.id, function(f)
@@ -42,9 +24,6 @@ menu.add_feature("Real Estate Scam", "action", moneyManu.id, function(f)
     ezBan()
 end)
 
-menu.add_feature("Real Estate Scam CUSTOM", "action", moneyManu.id, function(f)
-    ezBanCustom()
-end)
 
 menu.add_feature("Real Cargo Scam", "toggle", moneyManu.id, function(f)
   while f.on do
